913f945295e95cee80cecc1cdc661f1d90abed9f
Igor Kozlov <IKozlov2@alfabank.ru>
2025-06-04 15:44:34 +0300

    adding banner and version info

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.baybaka"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.3-SNAPSHOT"
 
 java {
     toolchain {
@@ -57,4 +57,8 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+springBoot {
+    buildInfo()
 }
